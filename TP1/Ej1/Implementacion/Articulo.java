12f250d4f9c11b043da37ef9ae57791647d68483
Aldana Rothamel <111920916+aldanarothamel@users.noreply.github.com>
2026-09-15 18:22:12 -0300

    Add Articulo class with constructor

---
+++ TP1/Ej1/Implementacion/Articulo.java
@@ -0,0 +1,9 @@
+public class Articulo {
+    private String codigo;
+    private String descripcion;
+
+    public Articulo(String codigo, String descripcion) {
+        this.codigo = codigo;
+        this.descripcion = descripcion;
+    }
+}
