883eaa1c317294522f7904c4120ace9f9969267d
Aldana Rothamel <111920916+aldanarothamel@users.noreply.github.com>
2026-09-15 18:45:55 -0300

    Add Banco class with transferir method

---
+++ TP1/Ej3/Implementacion/Banco.java
@@ -0,0 +1,5 @@
+public class Banco {
+    public void transferir(String cbuorigen, double importe) {
+        System.out.println("Transferencia bancaria desde CBU " + cbuorigen + " por $" + importe);
+    }
+}
