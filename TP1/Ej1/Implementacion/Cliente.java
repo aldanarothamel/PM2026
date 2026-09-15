66a166be616fe7f2073ad86cdb4bf5f26043c8eb
Aldana Rothamel <111920916+aldanarothamel@users.noreply.github.com>
2026-09-15 18:21:34 -0300

    Add Cliente class with id and nombre attributes

---
+++ TP1/Ej1/Implementacion/Cliente.java
@@ -0,0 +1,9 @@
+public class Cliente {
+    private String id;
+    private String nombre;
+
+    public Cliente(String id, String nombre) {
+        this.id = id;
+        this.nombre = nombre;
+    }
+}
